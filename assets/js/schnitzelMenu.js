864046378a910bbc5affc60812c63e92a9e5b44b
rensknoors <rensknoors@gmail.com>
2017-06-01 15:19:11 +0200

    schnitzelMenu hide verbeterd
    menu wordt verborgen als je naar het menu klikt

--- assets/js/schnitzelMenu.js
+++ assets/js/schnitzelMenu.js
@@ -1,16 +1,38 @@
+function showMenu() {
+    $('#schnitzelMenu').removeClass('closed');  
+    $('#schnitzelMenu').addClass('opened');
+    $('#schnitzelMenu').find('img').attr('src', 'assets/images/icons/close.svg');
+}
+
+function hideMenu() {
+    $('#schnitzelMenu').removeClass('opened');
+    $('#schnitzelMenu').addClass('closed');
+    $('#schnitzelMenu').find('img').attr('src', 'assets/images/icons/schnitzel.svg');
+}
+
 $('#schnitzelMenu').click( function() {
     $('#headerNav').toggleClass('enabled');
-    if ($(this).hasClass('opened')) {
-        $(this).removeClass('opened');
-        $(this).addClass('closed');
-        $(this).find('img').attr('src', 'assets/images/icons/schnitzel.svg');
+    if ($('#schnitzelMenu').hasClass('opened')) {
+        hideMenu();
     }
-    else if ($(this).hasClass('closed')) {
-        $(this).removeClass('closed');  
-        $(this).addClass('opened');
-        $(this).find('img').attr('src', 'assets/images/icons/close.svg');
+    else if ($('#schnitzelMenu').hasClass('closed')) {
+        showMenu();
     }
 });
+
+$(document).click(function(event) { 
+    if(!$(event.target).closest('#headerNav').length) {
+        if ($('#schnitzelMenu').hasClass('opened')) {
+            $('#headerNav').toggleClass('enabled');
+            hideMenu();
+        }
+    }
+});
+
+$('#schnitzelMenu').click(function(event){
+    event.stopPropagation();
+});
+
 
 var didScroll;
 var lastScrollTop = 0;
